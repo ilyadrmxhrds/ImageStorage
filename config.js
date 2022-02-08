08ab23eef0a96720d51dd806f2290344f256cf1a
IlyaDreamix <98055507+ilyadrmxhrds@users.noreply.github.com>
2022-02-08 19:59:29 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,49 @@
+/**
+ * Custom Amino WebSocket listening.
+ * Note: if you don't want to bother with this config and
+ * just want to listen messages, you need to
+ * change ``webSocket.on("message", {  })`` block.
+ * @param {Client} client Amino Client
+ * @param {SubClient} subClient Amino SubClient
+ * @author IlyaDreamix
+ */
+function customSocketListening(client, subClient) {
+    client.startListeningMessages();
+    client.ws.then((webSocket) => {
+        /**
+         * It's when everyting works
+         */
+         webSocket.on(
+            "open", () => {
+                if (client.debug) {
+                    console.log(`Amino WS connected: ${Date.now()}`);
+                }
+            }
+        );
+
+        /**
+         * It's when client got Amino message
+         */
+        webSocket.on(
+            "message", (message) => {
+                // Your code
+            }
+        );
+
+        /**
+         * It's when something does not work and it needs to restart
+         */
+        webSocket.on(
+            "close", () => {
+                if (client.debug) {
+                    console.log(`Amino WS closed: ${Date.now()}`);
+                }
+                customSocketListening(client, subClient);
+            }
+        );
+    });
+}
+
+module.exports = {
+    customSocketListening
+}
